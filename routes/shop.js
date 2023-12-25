277a9f4a8c155598948c65cd5342ad233fd036a7
vmujic1 <vmujic1@etf.unsa.ba>
2023-12-25 21:31:29 +0100

    Added products and routesexport

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,11 @@ const rootDir = require("../util/path");
 
 const router = express.Router();
 
+const adminData = require("./admin");
+
 router.get("/", (req, res, next) => {
   res.sendFile(path.join(rootDir, "views", "shop.html"));
+  console.log(adminData.products);
 });
 
 module.exports = router;
